852c444d0ebba0f50d0fc7713159297d573a35c7
Keginold Philip <keginoldp@hotmail.com>
2021-01-27 13:07:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {|call| call.length > 4}# Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |list|
+    chesse.include?(list)
 end
