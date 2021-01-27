f97e7d5f0da0bf82c57f4897ff49358ff73bba03
Keginold Philip <keginoldp@hotmail.com>
2021-01-27 12:29:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index.map do |name, index|
+    puts "#{index + 1} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
