264c6eb30edbb8f85e0b9ef1ef19292967bbb638
Keginold Philip <keginoldp@hotmail.com>
2021-01-27 13:03:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  if array.length > 4
-    return true
-  else
-    false# Your code here
+  array.any? {|call| call.length > 4}# Your code here
 end
 
 def find_the_cheese# code an argument here
