4bf48af0f4bfe21477bf123b636e7241db2248b6
Keginold Philip <keginoldp@hotmail.com>
2021-01-27 12:29:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index.map do |name, index|
-    puts "#{index + 1} #{name}"
+  array.each_with_index.map(1) do |name, index|
+    puts "#{index}. #{name}"
   end
 end
 
